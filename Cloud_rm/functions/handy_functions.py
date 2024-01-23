2a2893c9fa5588e1c6266b3028204ae5901b9b3f
filipronn <filipronn@gmail.com>
2024-01-23 14:03:40 +0100

    Save and load model functions

--- Cloud_rm/functions/handy_functions.py
+++ Cloud_rm/functions/handy_functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 
 def normalise_input_df(df,labels):
     #Normalise to zero mean unit variance for all given column labels
@@ -10,3 +11,16 @@ def normalise_input_df(df,labels):
         df[col]=(df[col]-tmp_mean)/np.sqrt(tmp_var)
     
     return df
+
+def save_model_and_test_data(filepath,model,X_test,y_test):
+    
+    model.save(filepath=filepath)
+    X_test.to_csv(filepath+'/xtest.csv',index=False)
+    y_test.to_csv(filepath+'/ytest.csv',index=False)
+
+def load_model_and_test_data(filepath):
+    model=tf.keras.models.load_model(filepath)
+    X_test=pd.read_csv(filepath+'/xtest.csv')
+    y_test=pd.read_csv(filepath+'/ytest.csv')
+
+    return model, X_test, y_test
