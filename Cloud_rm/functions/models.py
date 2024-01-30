0fcd37ac90d70445e1c30a0324a15b8057206242
filipronn <filipronn@gmail.com>
2024-01-30 14:36:40 +0100

    Created a simple 5 layer MLP similar to previous paper.

--- Cloud_rm/functions/models.py
+++ Cloud_rm/functions/models.py
@@ -55,4 +55,39 @@ def train_simple_model(df,x_labels,y_labels,split,epochs,batch_size):
     
     return model, history_df, X_test, y_test
 
+def train_5layer_64neurons_model(df,x_labels,y_labels,split,epochs,batch_size):
+
+    ##Split data##
+    X=df[x_labels]
+    y=df[y_labels]
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=split[1])
+    X_train, X_val, y_train, y_val = train_test_split(X_train,y_train, test_size=split[2])
+
+    
+    ##Create model##
+    model=tf.keras.Sequential([
+        layers.Dense(32,input_dim=len(x_labels),activation='linear'),
+        layers.Dense(64,activation='relu'),
+        layers.Dense(64,activation='relu'),
+        layers.Dense(64,activation='relu'),
+        layers.Dense(len(y_labels),activation='linear')
+    ])
+
+    ##Compile model##
+    model.compile(
+        optimizer="adam",
+        loss='mse',
+        metrics=["mse"],
+        run_eagerly=True
+    )
+
+    ##Train model##
+    history=LossHistory()
+    model.fit(X_train,y_train,epochs=epochs,validation_data=(X_val,y_val),batch_size=batch_size,callbacks=[history])
+
+    #Make training history to a dataframe
+    history_df=pd.DataFrame(history.hist,index=None)
+    
+    return model, history_df, X_test, y_test
+
 
