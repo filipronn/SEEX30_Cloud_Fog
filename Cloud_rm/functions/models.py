5be818ce72657e0e9ae74c18b6c843e0c303ded5
filipronn <filipronn@gmail.com>
2024-02-06 10:46:14 +0100

    PCA non PCA comparioson on NN

--- Cloud_rm/functions/models.py
+++ Cloud_rm/functions/models.py
@@ -36,15 +36,16 @@ def split_data(X,y,split):
 def train_simple_model(df,x_labels,y_labels,split,epochs,batch_size):
 
     ##Split data##
+    ##Split data##
     X=df[x_labels]
     y=df[y_labels]
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=split[1])
-    X_train, X_val, y_train, y_val = train_test_split(X_train,y_train, test_size=split[2])
+
+    X_train, y_train, X_val, y_val, X_test, y_test=split_data(X,y,split=split)
     
     ##Create model##
     model=tf.keras.Sequential([
         layers.Dense(32,input_dim=len(x_labels),activation='linear'),
-        layers.Dense(128,activation='relu'),
+        layers.Dense(64,activation='relu'),
         #layers.Dense(128,activation='relu'),
         layers.Dense(len(y_labels),activation='linear')
     ])
