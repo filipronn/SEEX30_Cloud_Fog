a68fe979671e188aebc0622e45f16eb74f567e1c
filipronn <filipronn@gmail.com>
2024-02-08 16:46:09 +0100

    Added calculation of outrate to class

--- Cloud_rm/py_torch_qrnn_adapt/neural_model.py
+++ Cloud_rm/py_torch_qrnn_adapt/neural_model.py
@@ -84,6 +84,16 @@ class QuantileNetwork:
         PSNR = 10*np.log10(maxval/mse)
         
         return PSNR
+    
+    def calc_outrate(y_test,preds):
+        outcount = 0
+        for i in range(np.shape(y_test_np)[0]):
+            for j in range(np.shape(y_test_np)[1]):
+                if y_test_np[i,j] < preds[i,j,0] or y_test_np[i,j] > preds[i,j,2]:
+                    outcount = outcount +1
+
+        outrate = outcount/np.size(y_test)
+        return outrate
 
 def fit_quantiles(X, y, quantiles=0.5, lossfn = 'marginal',
                     nepochs=100, val_pct=0.1,
