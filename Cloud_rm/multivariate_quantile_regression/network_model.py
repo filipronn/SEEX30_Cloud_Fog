dbb9c33ad5f88570006c6bf3dddeb2db296e853c
root <root@localhost.localdomain>
2024-02-23 10:06:04 +0000

    Added verbose option and updating training plot

--- Cloud_rm/multivariate_quantile_regression/network_model.py
+++ Cloud_rm/multivariate_quantile_regression/network_model.py
@@ -16,6 +16,9 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 from tqdm import tqdm
 
+import matplotlib.pyplot as plt
+from IPython.display import display, clear_output
+
 
 class QuantileNetworkMM(nn.Module):
     def __init__(self,n_in,n_out,y_dim, X_means, X_stds, y_mean, y_std, seq, device, data_norm):
@@ -60,6 +63,8 @@ class QuantileNetworkMM(nn.Module):
             return out.data.cpu().numpy()
         
 
+        
+
 class QuantileNetwork():
     def __init__(self,quantiles,loss='quantile'):
         self.quantiles=quantiles
@@ -69,11 +74,13 @@ class QuantileNetwork():
         else:
             self.device=torch.device('cpu')
 
-    def fit(self, X, y, train_indices, validation_indices, batch_size, nepochs, sequence,lr=0.001,data_norm=False):
+    def fit(self, X, y, train_indices, validation_indices, batch_size, nepochs, sequence,lr=0.001,data_norm=False,verbose=True,plot_training=False):
         self.model,self.train_loss,self.val_loss = fit_quantiles(X, y, train_indices, validation_indices,
                                                                   quantiles=self.quantiles, batch_size=batch_size, 
                                                                   sequence=sequence, n_epochs=nepochs,
-                                                                  device=self.device,lr=lr,data_norm=data_norm)
+                                                                  device=self.device,lr=lr,data_norm=data_norm,
+                                                                  verbose=verbose,
+                                                                  plot_training=plot_training)
 
     def predict(self, X):
         return self.model.predict(X)
@@ -124,8 +131,10 @@ class QuantileNetwork():
         z = y_true[:,:,None] - y_pred
         loss = np.sum(np.maximum(quantiles[None,None]*z, (quantiles[None,None] - 1)*z))
         return loss/(np.shape(y_true)[0])
+    
 
-def fit_quantiles(X,y,train_indices,validation_indices,quantiles,n_epochs,batch_size,sequence,lr,data_norm,loss='quantile',file_checkpoints=True,device=torch.device('cuda')):
+
+def fit_quantiles(X,y,train_indices,validation_indices,quantiles,n_epochs,batch_size,sequence,lr,data_norm,loss='quantile',file_checkpoints=True,device=torch.device('cuda'),verbose=True,plot_training=False):
 
 
     X_mean=np.mean(X,axis=0,keepdims=True)
@@ -143,13 +152,18 @@ def fit_quantiles(X,y,train_indices,validation_indices,quantiles,n_epochs,batch_
         y_dim=1
 
 
-
     tquantiles = torch.tensor(quantiles,dtype=torch.float,device=device)
 
     #Initiate loss tracking
     train_losses=torch.zeros(n_epochs,device=device)
     val_losses=torch.zeros(n_epochs,device=device)
     val_losses[0]=10000000 #For finding lowest new validation error later
+
+    #Initiate training/validation plot
+    if plot_training:
+        plt.ion()
+        fig, ax=plt.subplots()
+        ax.legend(['Training Loss','Validation Loss'])
 
     model=QuantileNetworkMM(n_in,n_out,y_dim,X_mean,X_std,y_mean,y_std,seq=sequence,device=device,data_norm=data_norm)
 
@@ -172,6 +186,17 @@ def fit_quantiles(X,y,train_indices,validation_indices,quantiles,n_epochs,batch_
 
         return torch.max(tquantiles[None,None]*z, (tquantiles[None,None] - 1)*z)
     
+    def plot_loss():
+
+        ax.cla()
+        ax.plot(train_losses[:epoch].cpu().numpy())
+        ax.plot(val_losses[:epoch].cpu().numpy())
+        ax.legend(['Training Loss','Validation Loss'])
+        display(fig)
+        clear_output(wait=True)
+        
+        #plt.show()
+    
     if len(y.shape) == 1 or y.shape[1] == 1: #If Univariate
         lossfn = quantile_loss
     else:
@@ -180,16 +205,17 @@ def fit_quantiles(X,y,train_indices,validation_indices,quantiles,n_epochs,batch_
     X=torch.tensor(X,dtype=torch.float,device=device)    
 
     for epoch in range(n_epochs):
-        print('Epoch {}'.format(epoch+1))
-        sys.stdout.flush()
+        if verbose:
+            print('Epoch {}'.format(epoch+1))
+            sys.stdout.flush()
         
         noise=torch.randn(X.shape,device=device)
         means=torch.mean(X,dim=0)*0.03
         noise=noise*means.repeat((len(noise[:,0]),1))
         tX=X+noise
         if data_norm:
-            X_mean=torch.mean(tX,dim=0)
-            X_std=torch.std(tX,dim=0)
+            X_mean=torch.mean(tX,dim=0,keepdim=True)
+            X_std=torch.std(tX,dim=0,keepdim=True)
             tX = torch.tensor((tX - X_mean) / X_std,dtype=torch.float,device=device)
             tY = torch.tensor((y - y_mean) / y_std,dtype=torch.float,device=device)
         else:
@@ -198,29 +224,45 @@ def fit_quantiles(X,y,train_indices,validation_indices,quantiles,n_epochs,batch_
 
 
         train_loss = torch.tensor([0],dtype=torch.float,device=device)
-        
-        for batch in tqdm(batches(train_indices, batch_size, shuffle=True),
-                          total=int(np.ceil(len(train_indices)/batch_size)),
-                          desc="Batch number"):
-            
+        if verbose:
+            for batch in tqdm(batches(train_indices, batch_size, shuffle=True),
+                            total=int(np.ceil(len(train_indices)/batch_size)),
+                            desc="Batch number"):
+                
 
-            idx = torch.tensor(batch,dtype=torch.int64,device=device)
+                idx = torch.tensor(batch,dtype=torch.int64,device=device)
 
-            model.train() #Initialise train mode
-            model.zero_grad() #Reset gradient
+                model.train() #Initialise train mode
+                model.zero_grad() #Reset gradient
 
-            yhat = model(tX[idx]) #Run algorithm
+                yhat = model(tX[idx]) #Run algorithm
 
-            loss=lossfn(yhat,idx).sum() #Run loss function
-            loss.backward() #Calculate gradient
+                loss=lossfn(yhat,idx).sum() #Run loss function
+                loss.backward() #Calculate gradient
 
-            optimizer.step()
+                optimizer.step()
 
-            train_loss=train_loss+loss.data #Increment loss
+                train_loss=train_loss+loss.data #Increment loss
+        else:
+            for batch in batches(train_indices, batch_size, shuffle=True):
+
+                idx = torch.tensor(batch,dtype=torch.int64,device=device)
+
+                model.train() #Initialise train mode
+                model.zero_grad() #Reset gradient
+
+                yhat = model(tX[idx]) #Run algorithm
+
+                loss=lossfn(yhat,idx).sum() #Run loss function
+                loss.backward() #Calculate gradient
+
+                optimizer.step()
+
+                train_loss=train_loss+loss.data #Increment loss
 
 
         validation_loss = torch.tensor([0],dtype=torch.float,device=device)
-
+        
         for batch_idx, batch in enumerate(batches(val_indices, batch_size, shuffle=False)):
 
 
@@ -233,21 +275,26 @@ def fit_quantiles(X,y,train_indices,validation_indices,quantiles,n_epochs,batch_
 
             validation_loss=validation_loss+lossfn(yhat, idx).sum()
 
-        print('Training loss {}'.format((train_loss.data/float(len(train_indices))).data.cpu().numpy())+
-              ' Validation loss {}'.format((validation_loss.data/float(len(val_indices))).data.cpu().numpy()),
-              end=None)
-        sys.stdout.flush()
+        if verbose:
+            print('Training loss {}'.format((train_loss.data/float(len(train_indices))).data.cpu().numpy())+
+                ' Validation loss {}'.format((validation_loss.data/float(len(val_indices))).data.cpu().numpy()),
+                end=None)
+            sys.stdout.flush()
 
         train_loss = (train_loss.data/float(len(train_indices)))
         validation_loss = (validation_loss.data/float(len(val_indices)))
 
         if validation_loss[0]<torch.min(val_losses[val_losses!=0.0]):
             if file_checkpoints:
-                torch.save(model,'tmp_file')            
-            print("----New best validation loss---- {}".format(validation_loss.data.cpu().numpy()))
+                torch.save(model,'tmp_file')
+            if verbose:
+                print("----New best validation loss---- {}".format(validation_loss.data.cpu().numpy()))
 
         train_losses[epoch] = train_loss
         val_losses[epoch] = validation_loss
+
+        if plot_training:
+            plot_loss()
 
     if file_checkpoints:
         model=torch.load('tmp_file')
