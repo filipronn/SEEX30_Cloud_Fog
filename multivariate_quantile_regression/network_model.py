dacb1abf24e9bbe310a9aff87007d1581e5a0ac3
BrorLen <antonbz@outlook.com>
2024-02-27 15:38:23 +0000

    revert changes regarding lr decay

--- multivariate_quantile_regression/network_model.py
+++ multivariate_quantile_regression/network_model.py
@@ -19,14 +19,13 @@ from tqdm import tqdm
 
 
 class QuantileNetworkMM(nn.Module):
-    def __init__(self,n_out, y_mean, y_std, seq, device, data_norm):
+    def __init__(self,n_out, y_mean, y_std, seq, device):
         super(QuantileNetworkMM, self).__init__()
         self.y_mean = y_mean
         self.y_std = y_std
         self.n_out=n_out
         self.linear=seq
         self.device=device
-        self.data_norm=data_norm
 
         self.softplus = nn.Softplus()
 
@@ -47,14 +46,11 @@ class QuantileNetworkMM(nn.Module):
         self.eval()
         self.zero_grad()
         tX=torch.tensor(x,dtype=torch.float,device=self.device)
-        if self.data_norm:
-            tX_mean = torch.mean(tX,0)
-            tX_std = torch.std(tX,0)
-            tX = (tX-tX_mean)/tX_std
-            norm_out = self.forward(tX)
-            out = norm_out * self.y_std[...,None] + self.y_mean[...,None]
-        else:
-            out=self.forward(tX)
+        tX_mean = torch.mean(tX,0)
+        tX_std = torch.std(tX,0)
+        tX = (tX-tX_mean)/tX_std
+        norm_out = self.forward(tX)
+        out = norm_out.data.cpu() * self.y_std[...,None] + self.y_mean[...,None]
         return out.data.cpu().numpy()
 
 class QuantileNetwork():
@@ -66,12 +62,11 @@ class QuantileNetwork():
         else:
             self.device=torch.device('cpu')
 
-    def fit(self, X, y, train_indices, validation_indices, batch_size, nepochs, sequence,lr=0.001,data_norm=False,noise_ratio=0.03,decay=False,decay_gamma=0.5,decay_wait=5):
+    def fit(self, X, y, train_indices, validation_indices, batch_size, nepochs, sequence,lr=0.001,noise_ratio=0.03):
         self.model,self.train_loss,self.val_loss = fit_quantiles(X, y, train_indices, validation_indices,
                                                                   quantiles=self.quantiles, batch_size=batch_size, 
                                                                   sequence=sequence, n_epochs=nepochs,
-                                                                  device=self.device,lr=lr,data_norm=data_norm,noise_ratio=noise_ratio,
-                                                                  decay=decay,decay_gamma=decay_gamma,decay_wait=decay_wait)
+                                                                  device=self.device,lr=lr,noise_ratio=noise_ratio)
 
     def predict(self, X):
         return self.model.predict(X)
@@ -110,34 +105,33 @@ class QuantileNetwork():
         loss = np.sum(np.maximum(quantiles[None,None]*z, (quantiles[None,None] - 1)*z))
         return loss/(np.shape(y_true)[0])
 
-def fit_quantiles(X,y,train_indices,validation_indices,quantiles,n_epochs,batch_size,sequence,lr,data_norm,noise_ratio,
-                  decay,decay_gamma,decay_wait,loss='quantile',file_checkpoints=True,device=torch.device('cuda')):
+def fit_quantiles(X,y,train_indices,validation_indices,quantiles,n_epochs,batch_size,sequence,lr,noise_ratio,
+                  loss='quantile',file_checkpoints=True,device=torch.device('cuda')):
+    #Find variables for use in QuantileNetworkMM
     n_out=len(quantiles)
     y_mean=y.mean(axis=0, keepdims=True)
     y_std=y.std(axis=0, keepdims=True)
-
+    #Turn inputs to tensors
     tX = torch.tensor(X,dtype=torch.float,device=device)
     tY = torch.tensor(y,dtype=torch.float,device=device)
-
-    if data_norm:
-            tY_mean = torch.tensor(y_mean,dtype=torch.float,device=device)
-            tY_std = torch.tensor(y_std,dtype=torch.float,device=device)
-            tY = (tY-tY_mean)/tY_std
-
     tquantiles = torch.tensor(quantiles,dtype=torch.float,device=device)
-
+    #Normalize y
+    tY_mean = torch.tensor(y_mean,dtype=torch.float,device=device)
+    tY_std = torch.tensor(y_std,dtype=torch.float,device=device)
+    tY = (tY-tY_mean)/tY_std
+    #Normalize X into separate tensor for epoch-wise noise addition
+    tX_mean = torch.mean(tX,0)
+    tX_std = torch.std(tX,0)
+    tX_norm = (tX-tX_mean)/tX_std 
+    
     #Initiate loss tracking
     train_losses=torch.zeros(n_epochs,device=device)
     val_losses=torch.zeros(n_epochs,device=device)
     val_losses[0]=10000000 #For finding lowest new validation error later
 
-    model=QuantileNetworkMM(n_out,y_mean,y_std,seq=sequence,device=device,data_norm=data_norm)
+    model=QuantileNetworkMM(n_out,y_mean,y_std,seq=sequence,device=device)
 
     optimizer = optim.Adam(model.parameters(),lr=lr) #Set optimiser
-    if decay:
-        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=decay_gamma)
-        no_improv_ctr = 0
-    
     
     train_indices=np.sort(train_indices)
     val_indices=np.sort(validation_indices)
@@ -164,17 +158,13 @@ def fit_quantiles(X,y,train_indices,validation_indices,quantiles,n_epochs,batch_
         print('Epoch {}'.format(epoch+1))
         sys.stdout.flush()
 
-        #Add noise to X
+        #Add noise to tX
         tX_noisy = tX + torch.randn(tX.shape) * torch.mean(tX,dim=0)*noise_ratio
 
-        #Then normalize X if wanted
-        if data_norm:
-            tX_n_mean = torch.mean(tX_noisy,0)
-            tX_n_std = torch.std(tX_noisy,0)
-            tX_noisy = (tX_noisy-tX_n_mean)/tX_n_std
-            tX_mean = torch.mean(tX,0)
-            tX_std = torch.std(tX,0)
-            tX = (tX-tX_mean)/tX_std 
+        #Then normalize tX_noisy
+        tX_n_mean = torch.mean(tX_noisy,0)
+        tX_n_std = torch.std(tX_noisy,0)
+        tX_noisy = (tX_noisy-tX_n_mean)/tX_n_std
 
         train_loss = torch.tensor([0],dtype=torch.float,device=device)
         
@@ -208,7 +198,7 @@ def fit_quantiles(X,y,train_indices,validation_indices,quantiles,n_epochs,batch_
             model.eval() #Set evaluation mode
             model.zero_grad() #Reset gradient
 
-            yhat=model(tX[idx])
+            yhat=model(tX_norm[idx])
 
             validation_loss=validation_loss+lossfn(yhat, idx).sum()
 
@@ -225,16 +215,6 @@ def fit_quantiles(X,y,train_indices,validation_indices,quantiles,n_epochs,batch_
             if file_checkpoints:
                 torch.save(model,'tmp_file')            
             print("----New best validation loss---- {}".format(validation_loss.data.cpu().numpy()))
-            if decay:
-                no_improv_ctr = 0
-        elif decay:
-            no_improv_ctr = no_improv_ctr + 1
-            if no_improv_ctr % decay_wait == 0:
-                scheduler.step()
-                print("----No improvement, learning rate dropped---- ")
-                if no_improv_ctr == 10*decay_wait:
-                    print("---No improvement for too long, training ended early---")
-                    break
 
         train_losses[epoch] = train_loss
         val_losses[epoch] = validation_loss
