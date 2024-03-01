4b8e3159961f4c8f9256c6f2cbca0081638c95b2
root <root@localhost.localdomain>
2024-03-01 14:49:37 +0000

    Fixed the plot function

--- Cloud_rm/functions/plot_metrics.py
+++ Cloud_rm/functions/plot_metrics.py
@@ -6,7 +6,7 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 
 
-def plot_metrics(models,X_tests,y_tests,predictions,df,index_y=10,samples=100):
+def plot_metrics(models,X_tests,y_tests,predictions,df,nrows,ncols,index_y=10,samples=100):
     # models - array of models to plot from
     # X_test - array of test data for X
     # y_tests - array of test data fro y
@@ -37,24 +37,17 @@ def plot_metrics(models,X_tests,y_tests,predictions,df,index_y=10,samples=100):
     figs=[]
     axs=[]
 
-    ## Plot training and validation loss ##
-    if len(models)>=4:
-        nrows=2
-        ncols=np.ceil(len(models)/2)
-    else:
-        nrows=1
-        ncols=len(models)
 
     fig_1, ax_1=plt.subplots(nrows=nrows,ncols=ncols)
     fig_1.suptitle("Training/Validation loss")
 
-    for i, model in enumerate(models):
-        ax_1[i].plot(model.train_loss.data.cpu().numpy())
-        ax_1[i].plot(model.val_loss.data.cpu().numpy())
-        ax_1[i].set_title("All channels estimated")
-        ax_1[i].legend(['Training Loss','Validation Loss'])
-        ax_1[i].set_xlabel("Epochs")
-        ax_1[i].set_ylabel("Loss")
+    for i,ax in enumerate(ax_1.ravel()):
+        ax.plot(models[i].train_loss.data.cpu().numpy())
+        ax.plot(models[i].val_loss.data.cpu().numpy())
+        ax.set_title("All channels estimated")
+        ax.legend(['Training Loss','Validation Loss'])
+        ax.set_xlabel("Epochs")
+        ax.set_ylabel("Loss")
 
     figs.append(fig_1)
     axs.append(ax_1)
@@ -80,19 +73,19 @@ def plot_metrics(models,X_tests,y_tests,predictions,df,index_y=10,samples=100):
     fig_2,ax_2=plt.subplots(nrows=nrows,ncols=ncols)
     fig_2.suptitle("Residual plots.")
 
-    for i, model in enumerate(models):
-        ax_2[i].plot(y_true[i][indices_thin[i]],residuals[i][indices_thin[i]],'.', markersize=2)
-        ax_2[i].plot(y_true[i][indices_med[i]],residuals[i][indices_med[i]],'.', markersize=2)
-        ax_2[i].plot(y_true[i][indices_thick[i]],residuals[i][indices_thick[i]],'.', markersize=2)
-        ax_2[i].set_title("Model "+str(i))
-        ax_2[i].hlines(0,xmin=-1,xmax=10,colors='r')
-        ax_2[i].set_xlim((np.min(y_true[i])-0.2,np.max(y_true[i])+0.2))
-        ax_2[i].legend(['Residual thin COT <3.6',
+    for i,ax in enumerate(ax_2.ravel()):
+        ax.plot(y_true[i][indices_thin[i]],residuals[i][indices_thin[i]],'.', markersize=2)
+        ax.plot(y_true[i][indices_med[i]],residuals[i][indices_med[i]],'.', markersize=2)
+        ax.plot(y_true[i][indices_thick[i]],residuals[i][indices_thick[i]],'.', markersize=2)
+        ax.set_title("Model "+str(i))
+        ax.hlines(0,xmin=-1,xmax=10,colors='r')
+        ax.set_xlim((np.min(y_true[i])-0.2,np.max(y_true[i])+0.2))
+        ax.legend(['Residual thin COT <3.6',
                         'Residual med COT <23',
                         'Residual thick COT <50',
                         'Zero error line'])
-        ax_2[i].set_xlabel("Ground Truth")
-        ax_2[i].set_ylabel("Residual")
+        ax.set_xlabel("Ground Truth")
+        ax.set_ylabel("Residual")
 
 
     figs.append(fig_2)
@@ -101,20 +94,20 @@ def plot_metrics(models,X_tests,y_tests,predictions,df,index_y=10,samples=100):
     ## Plot prediction v ground truth ##
     fig_3,ax_3=plt.subplots(nrows=nrows,ncols=ncols)
     fig_3.suptitle("Prediction vs Ground Truth")
-    for i, model in enumerate(models):
+    for i,ax in enumerate(ax_3.ravel()):
         
-        ax_3[i].plot(y_pred[i][indices_thin[i],1],y_true[i][indices_thin[i]],'.', markersize=1)
-        ax_3[i].plot(y_pred[i][indices_med[i],1],y_true[i][indices_med[i]],'.', markersize=1)
-        ax_3[i].plot(y_pred[i][indices_thick[i],1],y_true[i][indices_thick[i]],'.', markersize=1)
+        ax.plot(y_pred[i][indices_thin[i],1],y_true[i][indices_thin[i]],'.', markersize=1)
+        ax.plot(y_pred[i][indices_med[i],1],y_true[i][indices_med[i]],'.', markersize=1)
+        ax.plot(y_pred[i][indices_thick[i],1],y_true[i][indices_thick[i]],'.', markersize=1)
         #plt.plot(cloudy_sort,'.')
         line=np.linspace(0,1,100)
-        ax_3[i].plot(line,line)
-        ax_3[i].legend(['Predictions thin COT <3.6',
+        ax.plot(line,line)
+        ax.legend(['Predictions thin COT <3.6',
                         'Predictions med COT <23',
                         'Predictions thick COT <50',
                         'Zero Error line'])
-        ax_3[i].set_xlabel("Prediction")
-        ax_3[i].set_ylabel("Ground Truth")
+        ax.set_xlabel("Prediction")
+        ax.set_ylabel("Ground Truth")
 
     figs.append(fig_3)
     axs.append(ax_3)
@@ -124,7 +117,7 @@ def plot_metrics(models,X_tests,y_tests,predictions,df,index_y=10,samples=100):
     
     fig_4,ax_4=plt.subplots(nrows=nrows,ncols=ncols)
     fig_4.suptitle("Prediction vs Ground Truth, averaged")
-    for i, model in enumerate(models):
+    for i,ax in enumerate(ax_4.ravel()):
         freq_true=np.zeros(len(bins))
         freq_pred=np.zeros(len(bins))
         for j,edge in enumerate(bins):
@@ -135,13 +128,13 @@ def plot_metrics(models,X_tests,y_tests,predictions,df,index_y=10,samples=100):
                 
                 freq_true[j]=mean_bin_true
                 freq_pred[j]=mean_bin_pred
-        ax_4[i].plot(freq_pred,freq_true,'.')
+        ax.plot(freq_pred,freq_true,'.')
         #plt.plot(cloudy_sort,'.')
         line=np.linspace(0,1,100)
-        ax_4[i].plot(line,line)
-        ax_4[i].legend(['Predictions'])
-        ax_4[i].set_xlabel("Prediction")
-        ax_4[i].set_ylabel("Ground Truth")
+        ax.plot(line,line)
+        ax.legend(['Predictions'])
+        ax.set_xlabel("Prediction")
+        ax.set_ylabel("Ground Truth")
 
     figs.append(fig_4)
     axs.append(ax_4)
@@ -167,20 +160,20 @@ def plot_metrics(models,X_tests,y_tests,predictions,df,index_y=10,samples=100):
     # Plot the values
     fig_5, ax_5 = plt.subplots(nrows=nrows,ncols=ncols)
     fig_5.suptitle("Reflectivity and uncertainty")
-    for i, model in enumerate(models):
-        ax_5[i].plot(y_true_sort_samp[i],'.',label='Ground Truth')
-        ax_5[i].errorbar(x=np.linspace(0,len(y_pred_sort_samp[i][:,1]),len(y_pred_sort_samp[i][:,1]))
+    for i,ax in enumerate(ax_5.ravel()):
+        ax.plot(y_true_sort_samp[i],'.',label='Ground Truth')
+        ax.errorbar(x=np.linspace(0,len(y_pred_sort_samp[i][:,1]),len(y_pred_sort_samp[i][:,1]))
                     ,y=y_pred_sort_samp[i][:,1],
                     yerr=[np.abs(y_pred_sort_samp[i][:,1]-y_pred_sort_samp[i][:,0]),
                         np.abs(y_pred_sort_samp[i][:,1]-y_pred_sort_samp[i][:,2])],
                             marker='.',fmt='.',label='Predictions')
 
         percent=0.1
-        ax_5[i].plot(y_true_sort_samp[i]-y_true_sort_samp[i]*percent,'g',label='percent')
-        ax_5[i].plot(y_true_sort_samp[i]+y_true_sort_samp[i]*percent,'g',label='percent')
-        ax_5[i].set_xlabel("Arbitrary samples")
-        ax_5[i].set_ylabel("Reflectivity")
-        ax_5[i].legend()
+        ax.plot(y_true_sort_samp[i]-y_true_sort_samp[i]*percent,'g',label='percent')
+        ax.plot(y_true_sort_samp[i]+y_true_sort_samp[i]*percent,'g',label='percent')
+        ax.set_xlabel("Arbitrary samples")
+        ax.set_ylabel("Reflectivity")
+        ax.legend()
 
     figs.append(fig_5)
     axs.append(ax_5)
